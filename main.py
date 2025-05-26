4d46f7994a1d1c01e191e40c039dda1fc73b99c1
SomkamolV <somkamolv@hotmail.com>
2025-05-26 16:13:54 +0700

    main -> section 112

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import another_module
+# import another_module
+#
+# print(another_module.another_variable)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.forward(50)
+# timmy.right(45)
+# timmy.forward(20)
+# timmy.color("DeepPink2")
+# print(timmy)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# print(my_screen.canvwidth)
 
-print(another_module.another_variable)
-from turtle import Turtle, Screen
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.forward(50)
-timmy.right(45)
-timmy.forward(20)
-timmy.color("DeepPink2")
-print(timmy)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-print(my_screen.canvwidth)
+from prettytable import PrettyTable, TableStyle
 
-my_screen.exitonclick()
+table = PrettyTable()
+
+table.add_column("Pokemon name", ["Pikachu","Squirtle","Charmander"])
+table.add_column("Type", ["Electric","Water","Fire"])
+table.align = "l"
+
+print(table)
+
+
