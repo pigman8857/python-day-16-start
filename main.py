2b68c9ccd87335c4e2f0475e8b98d6c49145dc30
SomkamolV <somkamolv@hotmail.com>
2025-05-26 14:57:36 +0700

    main -> section 110

---
+++ main.py
@@ -0,0 +1,17 @@
+import another_module
+
+print(another_module.another_variable)
+from turtle import Turtle, Screen
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.forward(50)
+timmy.right(45)
+timmy.forward(20)
+timmy.color("DeepPink2")
+print(timmy)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+print(my_screen.canvwidth)
+
+my_screen.exitonclick()
